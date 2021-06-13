2c7f9368693717f9b3ef7fd9686a74844d6b5126
Your Name <you@example.com>
2021-06-13 16:54:59 +0300

    first commit

---
+++ FileSubStringSearcher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace FileSubStringSearcher
+{
+    class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку для поиска в файлах:");
+            var pattern = Console.ReadLine();
+
+            var startPath = ConfigurationSettings.AppSettings["startFolder"];
+            var filePathes = new List<string>();
+
+            var getSupTreeFiles = Directory
+                .GetFiles(startPath, ConfigurationSettings.AppSettings["extensions"], SearchOption.AllDirectories);
+
+            foreach(var path in getSupTreeFiles)            
+                if (File.ReadAllText(path).Contains(pattern))                
+                    filePathes.Add(new FileInfo(path).FullName);
+
+            Console.WriteLine("\r\nСписок файлов содержащих строку:");
+
+            if (filePathes.Count == 0)
+                Console.WriteLine("Не найдено");
+            else
+                foreach (var item in filePathes)
+                    Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+    }
+}
